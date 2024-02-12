0abe4fc8d82bb2c6b71f404510a3bb54f2cc4393
Harjit Singh Brar <workandresearchbrar@gmail.com>
2024-02-12 17:46:46 +0800

    new: added middleware for auth to refresh access tokens [1004]

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,7 +1,52 @@
 const passport = require('passport');
 const httpStatus = require('http-status');
+const moment = require('moment');
+const { google } = require('googleapis');
 const ApiError = require('../utils/ApiError');
 const { roleRights } = require('../config/roles');
+const { oauth } = require('../config/config');
+const { authService } = require('../services');
+
+const oauth2Client = new google.auth.OAuth2(oauth.clientID, oauth.clientSecret, oauth.callbackURL);
+
+const refreshCallback = (req, resolve, reject) => async (err, tokens) => {
+  if (err) {
+    return reject(new ApiError(httpStatus.UNAUTHORIZED, 'Please authenticate'));
+  }
+
+  req.user.account = {
+    ...req.user.account,
+    accessToken: tokens.access_token,
+    expireDate: tokens.expiry_date,
+  };
+  const { account } = req.user;
+  await authService.updateUserTokens(req.user, account.provider, account.subject);
+
+  resolve();
+};
+
+const refresh = async (req, res, next) => {
+  // session expire is the master lifespan of an authentication flow
+  const isAuthenticated = !req.user || !req.session || !req.session.passport || !req.session.passport.user;
+  if (isAuthenticated) {
+    return next(new ApiError(httpStatus.UNAUTHORIZED, 'Please authenticate'));
+  }
+
+  if (moment().subtract(req.user.account.expireDate, 's').format('X') > -300) {
+    oauth2Client.setCredentials({
+      access_token: req.user.account.accessToken,
+      refresh_token: req.user.account.refreshToken,
+    });
+
+    return new Promise((resolve, reject) => {
+      oauth2Client.refreshAccessToken(refreshCallback(req, resolve, reject));
+    })
+      .then(() => next())
+      .catch((err) => next(err));
+  }
+
+  next();
+};
 
 const verifyCallback = (req, resolve, reject, requiredRights) => async (err, user, info) => {
   if (err || info || !user) {
@@ -30,4 +75,7 @@ const auth =
       .catch((err) => next(err));
   };
 
-module.exports = auth;
+module.exports = {
+  auth,
+  refresh,
+};
