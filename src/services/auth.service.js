2854da32df313e3cd30cfba8c818288ce77923b7
Harjit Singh Brar <workandresearchbrar@gmail.com>
2024-02-12 17:44:57 +0800

    refactor: added methods to update user model for authentication [1004]

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,99 +1,32 @@
-const httpStatus = require('http-status');
-const tokenService = require('./token.service');
 const userService = require('./user.service');
-const Token = require('../models/token.model');
-const ApiError = require('../utils/ApiError');
-const { tokenTypes } = require('../config/tokens');
 
 /**
- * Login with username and password
- * @param {string} email
- * @param {string} password
- * @returns {Promise<User>}
+ * Creates a user if it doesn't exist or updates its token, otherwise
+ * @param {*} user - user object with new tokens
+ * @param {*} provider - provider for the oauth, default: google
+ * @param {*} subject - id of the user given by provider
+ * @param {*} tokens - new tokens
  */
-const loginUserWithEmailAndPassword = async (email, password) => {
-  const user = await userService.getUserByEmail(email);
-  if (!user || !(await user.isPasswordMatch(password))) {
-    throw new ApiError(httpStatus.UNAUTHORIZED, 'Incorrect email or password');
-  }
-  return user;
-};
-
-/**
- * Logout
- * @param {string} refreshToken
- * @returns {Promise}
- */
-const logout = async (refreshToken) => {
-  const refreshTokenDoc = await Token.findOne({ token: refreshToken, type: tokenTypes.REFRESH, blacklisted: false });
-  if (!refreshTokenDoc) {
-    throw new ApiError(httpStatus.NOT_FOUND, 'Not found');
-  }
-  await refreshTokenDoc.remove();
-};
-
-/**
- * Refresh auth tokens
- * @param {string} refreshToken
- * @returns {Promise<Object>}
- */
-const refreshAuth = async (refreshToken) => {
-  try {
-    const refreshTokenDoc = await tokenService.verifyToken(refreshToken, tokenTypes.REFRESH);
-    const user = await userService.getUserById(refreshTokenDoc.user);
-    if (!user) {
-      throw new Error();
-    }
-    await refreshTokenDoc.remove();
-    return tokenService.generateAuthTokens(user);
-  } catch (error) {
-    throw new ApiError(httpStatus.UNAUTHORIZED, 'Please authenticate');
+const createOrUpdateUserTokens = async (user, provider, subject, tokens) => {
+  const currUser = await userService.getUserByProviderAndSubject(provider, subject);
+  if (!currUser) {
+    await userService.createUser(user);
+  } else if (currUser.account.accessTokens !== tokens.accessTokens) {
+    await userService.findUserAndUpdate(provider, subject, user);
   }
 };
 
 /**
- * Reset password
- * @param {string} resetPasswordToken
- * @param {string} newPassword
- * @returns {Promise}
+ * Updates user's tokens for oauth
+ * @param {*} user - user object with tokens
+ * @param {*} provider - provider for the oauth, default: google
+ * @param {*} subject - id of the user given by the provider
  */
-const resetPassword = async (resetPasswordToken, newPassword) => {
-  try {
-    const resetPasswordTokenDoc = await tokenService.verifyToken(resetPasswordToken, tokenTypes.RESET_PASSWORD);
-    const user = await userService.getUserById(resetPasswordTokenDoc.user);
-    if (!user) {
-      throw new Error();
-    }
-    await userService.updateUserById(user.id, { password: newPassword });
-    await Token.deleteMany({ user: user.id, type: tokenTypes.RESET_PASSWORD });
-  } catch (error) {
-    throw new ApiError(httpStatus.UNAUTHORIZED, 'Password reset failed');
-  }
-};
-
-/**
- * Verify email
- * @param {string} verifyEmailToken
- * @returns {Promise}
- */
-const verifyEmail = async (verifyEmailToken) => {
-  try {
-    const verifyEmailTokenDoc = await tokenService.verifyToken(verifyEmailToken, tokenTypes.VERIFY_EMAIL);
-    const user = await userService.getUserById(verifyEmailTokenDoc.user);
-    if (!user) {
-      throw new Error();
-    }
-    await Token.deleteMany({ user: user.id, type: tokenTypes.VERIFY_EMAIL });
-    await userService.updateUserById(user.id, { isEmailVerified: true });
-  } catch (error) {
-    throw new ApiError(httpStatus.UNAUTHORIZED, 'Email verification failed');
-  }
+const updateUserTokens = async (user, provider, subject) => {
+  await userService.findUserAndUpdate(provider, subject, user);
 };
 
 module.exports = {
-  loginUserWithEmailAndPassword,
-  logout,
-  refreshAuth,
-  resetPassword,
-  verifyEmail,
+  createOrUpdateUserTokens,
+  updateUserTokens,
 };
