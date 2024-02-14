a09eef3a4fe0dc44c0a90242ecdfcafa50d062ad
Harjit Singh Brar <workandresearchbrar@gmail.com>
2024-02-14 23:42:24 +0800

    refactor: cascaded the changes of roles [10011]

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -18,7 +18,7 @@ const userSchema = mongoose.Schema(
     role: {
       type: String,
       enum: roles,
-      default: roleNames.user,
+      required: true,
     },
     account: accountSchema,
   },
