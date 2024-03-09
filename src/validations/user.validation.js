edcad9479f3a177f68e317dec274ba46c3bb2821
Harjit Singh Brar <workandresearchbrar@gmail.com>
2024-03-09 12:05:38 +0800

    new: added constraints to user route [10018]

--- src/validations/user.validation.js
+++ src/validations/user.validation.js
@@ -2,13 +2,15 @@ const Joi = require('joi');
 
 const createUser = {
   body: Joi.object().keys({
-    firstName: Joi.string().required(),
-    lastName: Joi.string().required(),
-    email: Joi.string().required().email(),
-    location: Joi.object().required().keys({
-      city: Joi.string().required(),
-      state: Joi.string().required(),
-    }),
+    firstName: Joi.string().min(1).max(25).required(),
+    lastName: Joi.string().min(1).max(25).required(),
+    email: Joi.string().max(40).required().email(),
+    location: Joi.object()
+      .required()
+      .keys({
+        city: Joi.string().min(1).max(25).required(),
+        state: Joi.string().min(1).max(25).required(),
+      }),
   }),
 };
 
