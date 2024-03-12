37fe12446b9d1f0a003f2a5712ed3def479a589f
Harjit Singh Brar <workandresearchbrar@gmail.com>
2024-03-13 00:24:59 +0800

    new: added controller for updating user [10022]

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,7 @@
 const httpStatus = require('http-status');
 const catchAsync = require('../utils/catchAsync');
 const { userService, locationService } = require('../services');
+const pick = require('../utils/pick');
 
 const createUser = catchAsync(async (req, res) => {
   const location = await locationService.findLocation(req.body.location, true);
@@ -10,6 +11,13 @@ const createUser = catchAsync(async (req, res) => {
   res.status(httpStatus.CREATED).send(user);
 });
 
+const updateUser = catchAsync(async (req, res) => {
+  const filter = pick(req.query, ['id']);
+  const result = await userService.updateUser(filter, req.body);
+  res.status(httpStatus.OK).send(result);
+});
+
 module.exports = {
   createUser,
+  updateUser,
 };
