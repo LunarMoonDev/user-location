158f0809ddf8ff3442046b6093bda6916dfccc0f
Harjit Singh Brar <workandresearchbrar@gmail.com>
2024-03-13 00:24:41 +0800

    new: added update user validation [10022]

--- src/validations/user.validation.js
+++ src/validations/user.validation.js
@@ -1,4 +1,5 @@
 const Joi = require('joi');
+const { roles } = require('../config/roles');
 
 const createUser = {
   body: Joi.object().keys({
@@ -14,6 +15,23 @@ const createUser = {
   }),
 };
 
+const updateUser = {
+  body: Joi.object()
+    .keys({
+      firstName: Joi.string().min(1).max(25),
+      lastName: Joi.string().min(1).max(25),
+      email: Joi.string().max(40).email(),
+      role: Joi.string().valid(...roles),
+      location: Joi.object().keys({
+        city: Joi.string().min(1).max(25).required(),
+        state: Joi.string().min(1).max(25).required(),
+      }),
+    })
+    .required()
+    .min(1),
+};
+
 module.exports = {
   createUser,
+  updateUser,
 };
