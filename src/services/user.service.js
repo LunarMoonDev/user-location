2848cf77a757ccf97099db3db506e70fd20aabe0
Harjit Singh Brar <workandresearchbrar@gmail.com>
2024-03-16 11:29:30 +0800

    new: added queryUser method for get users endpoint [10014]

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -66,9 +66,24 @@ const updateUser = async (filter, user) => {
   return updatedUser;
 };
 
+/**
+ * Query for users
+ * @param {Object} filter - Mongo filter
+ * @param {Object} options - Query options
+ * @param {string} [options.sortBy] - Sort option in the format: sortField:(desc|asc)
+ * @param {number} [options.limit] - Maximum number of results per page (default = 10)
+ * @param {number} [options.page] - Current page (default = 1)
+ * @returns {Promise<QueryResult>}
+ */
+const queryUsers = async (filter, options) => {
+  const users = await User.paginate(filter, { ...options, populate: 'location' });
+  return users;
+};
+
 module.exports = {
   createUser,
   getUserByProviderAndSubject,
   findUserAndUpdate,
   updateUser,
+  queryUsers,
 };
