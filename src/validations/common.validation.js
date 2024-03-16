0be368aa8b7f0e05f80eaecae5ebd2fedeb9705d
Harjit Singh Brar <workandresearchbrar@gmail.com>
2024-03-16 23:26:42 +0800

    new: added validation for delete filter [10023]

--- src/validations/common.validation.js
+++ src/validations/common.validation.js
@@ -8,6 +8,15 @@ const queryFilter = {
     .required(),
 };
 
+const deleteFilter = {
+  query: Joi.object()
+    .keys({
+      ids: Joi.array().min(1).items(Joi.string().hex().length(24)).single().required(),
+    })
+    .required(),
+};
+
 module.exports = {
   queryFilter,
+  deleteFilter,
 };
